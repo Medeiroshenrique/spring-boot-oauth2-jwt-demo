5d00491ac62395b5c85563432ae52eeacb4fed33
Henrique  Medeiros <henrique.c.r.medeiros@gmail.com>
2026-04-28 11:08:46 -0300

    Brcrypt was already added, just cleaning the DemoApplication class for organization criterea

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner {
+public class DemoApplication {
 
 	@Autowired
 	private PasswordEncoder passwordEncoder;
@@ -16,12 +16,4 @@ public class DemoApplication implements CommandLineRunner {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("ENCODE: " + passwordEncoder.encode("123456"));
-
-		//o que acontece por baixo dos panos
-		boolean result = passwordEncoder.matches("123456","$2a$10$YnBL3pqas7sb.6DIKuIwYe9iY65tNU4i2u.CcKeWAxnd3S5kCYoBa");
-		System.out.println(result);
-	}
 }
