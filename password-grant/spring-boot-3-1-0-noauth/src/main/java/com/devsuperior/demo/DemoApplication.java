6d2d7b7374ee0ad3ff28b06ea194d203a168c15e
Henrique  Medeiros <henrique.c.r.medeiros@gmail.com>
2026-04-28 11:00:19 -0300

    Brcrypt enconding under the hoods

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,13 +1,27 @@
 package com.devsuperior.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("ENCODE: " + passwordEncoder.encode("123456"));
+
+		//o que acontece por baixo dos panos
+		boolean result = passwordEncoder.matches("123456","$2a$10$YnBL3pqas7sb.6DIKuIwYe9iY65tNU4i2u.CcKeWAxnd3S5kCYoBa");
+		System.out.println(result);
+	}
 }
